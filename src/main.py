f65c603602e52e8008339d6865ad723947436c0e
Luci-dv <threshhy102@gmail.com>
2025-05-14 00:29:45 +0100

    Public hosting enabled

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from block_to_html import markdown_to_html_node
 from text_parser import extract_title
+import sys
 
 def copy_static(source_dir, dest_dir):
     if os.path.exists(dest_dir):
@@ -22,7 +23,7 @@ def copy_static(source_dir, dest_dir):
         else:
             copy_static(source_path, dest_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path}")
     with open(from_path, "r", encoding="utf-8") as file:
         markdown_content = file.read()
@@ -33,12 +34,14 @@ def generate_page(from_path, template_path, dest_path):
         template = file.read()
     title_add = template.replace("{{ Title }}", title)
     final = title_add.replace("{{ Content }}", html)
+    final = final.replace('href="/', f'href="{basepath}')
+    final = final.replace('src"/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as file:
-        file.write(final)
+        file.write(final)   
     print(f"File created at {dest_path}")
 
-def generate_all_pages(content_dir, template_path, public_dir):
+def generate_all_pages(content_dir, template_path, public_dir, basepath):
     for root, _, files in os.walk(content_dir):
         for file in files:
             if file.endswith(".md"):
@@ -46,11 +49,15 @@ def generate_all_pages(content_dir, template_path, public_dir):
                 rel_path = os.path.relpath(markdown_path, content_dir)
                 rel_path_html = os.path.splitext(rel_path)[0] + ".html"
                 html_path = os.path.join(public_dir, rel_path_html)
-                generate_page(markdown_path, template_path, html_path)
+                generate_page(markdown_path, template_path, html_path, basepath)
 
 def main():
-    copy_static("static", "public")
-    generate_all_pages("content", "template.html", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    copy_static("static", "docs")
+    generate_all_pages("content", "template.html", "docs", basepath)
 
 # Call the main function when the script is run
 if __name__ == "__main__":
